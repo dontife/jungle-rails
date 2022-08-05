08685e2938cdfd471504fc2460a2a51b0d789990
Boluwatife Ade ogunlola <dontife@ymail.com>
2022-08-05 15:22:40 -0400

    add basic admin  Authentication

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,6 @@
 class ProductsController < ApplicationController
 
+  http_basic_authenticate_with name: ENV["ADMIN_NAME"], password: ENV["ADMIN_PASSWORD"]
   def index
     @products = Product.all.order(created_at: :desc)
   end
